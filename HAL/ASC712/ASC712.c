4c4645ee681f74387bb9421f52b598bc45cf190a
AhmedAlyEl-Ghannam <ahmedaly.g112@gmail.com>
2023-09-03 18:26:56 +0300

    Wrote APIs && Configuration macros for ASC712

--- HAL/ASC712/ASC712.c
+++ HAL/ASC712/ASC712.c
@@ -2,3 +2,18 @@
 #include "../../LIB/STD_Types.h"
 #include "../../MCAL/ADC/ADC.h"
 #include "ASC712.h"
+
+void ASC712_voidInit(void)
+{
+	
+}
+
+u16 ASC712_u16GetCurrentReadingDC(void)
+{
+	
+}
+
+u16 ASC712_u16GetCurrentReadingAC(void)
+{
+	
+}
--- HAL/ASC712/ASC712.h
+++ HAL/ASC712/ASC712.h
@@ -2,5 +2,15 @@
 #ifndef ASC712_H_
 #define ASC712_H_
 
+/*******************************CONFIG******************************/
+
+#define ASC712_ADC_CHANNEL		ADC_CHANNAL_0
+#define ASC712_ADC_PRESCALER	ADC_DIV_64
+
+/****************************INTERFACE******************************/
+
+void ASC712_voidInit(void);
+u16 ASC712_u16GetCurrentReadingDC(void);
+u16 ASC712_u16GetCurrentReadingAC(void);
 
 #endif
