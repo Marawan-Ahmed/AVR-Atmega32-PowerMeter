62a12582baf2e936ea81195c32af7d6168d944ac
Marawan-Ahmed <81922808+Marawan-Ahmed@users.noreply.github.com>
2023-09-05 16:39:17 +0300

    removed rms

--- APP/calculations.c
+++ APP/calculations.c
@@ -10,16 +10,16 @@
 u32	Global_u32SampleCount; //used for True RMS
 
 f64	Global_f64InstVoltage_mv;
-f64	Global_f64SumVoltage_mv; //used for True RMS
-f64	Global_f64RMSVoltage_mv; //used for True RMS
+//f64	Global_f64SumVoltage_mv; //used for True RMS
+//f64	Global_f64RMSVoltage_mv; //used for True RMS
 
 f64	Global_f64InstCurrent_mA;
-f64	Global_f64SumCurrent_mA; //used for True RMS
-f64	Global_f64RMSCurrent_mA; //used for True RMS
+//f64	Global_f64SumCurrent_mA; //used for True RMS
+//f64	Global_f64RMSCurrent_mA; //used for True RMS
 
 f64	Global_f64InstPower_mW;
-f64	Global_f64SumPower_mA; //used for True RMS
-f64	Global_f64RMSPower_mA; //used for True RMS
+//f64	Global_f64SumPower_mA; //used for True RMS
+//f64	Global_f64RMSPower_mA; //used for True RMS
 
 f64	Global_f64Energy_mJ;
 
@@ -41,12 +41,12 @@ void CALC_voidInit(void){
 void CALC_voidPeriodicFunc(void){
 	static u16 Local_u16Count =0;
 	//enter this condition every SMAPLING_PERIOD_mS 
-	if(Local_u16Count % 4 == 0){ ////////////////////?????????????????????????????????
+	if(Local_u16Count == 4 * SMAPLING_PERIOD_mS){ ////////////////////?????????????????????????????????
 		//read inst volt
 		Global_f64InstVoltage_mv = VSEN_f32GetVoltageReading();
 		//read inst current
 		Global_f64InstCurrent_mA = ACS712_f32GetCurrentReading();
-
+/*
 		//Claculating RMS Values
 		Global_f64SumVoltage_mv += Global_f64InstVoltage_mv * Global_f64InstVoltage_mv; //Calculate Voltage Sum Square across SMAPLING_COUNT samples
 		Global_f64SumCurrent_mA += Global_f64InstCurrent_mA * Global_f64InstCurrent_mA; //Calculate Current Sum Square across SMAPLING_COUNT samples
@@ -58,23 +58,39 @@ void CALC_voidPeriodicFunc(void){
 			Global_u32SampleCount = 0; 													   	//Reset Sampling
 			LED_voidToggleLEDSingle(DIO_PORTD, DIO_PIN7);
 		}	
-		
+*/		
 		Global_f64InstPower_mW = Global_f64InstVoltage_mv * Global_f64InstCurrent_mA / 1000; //Power(mJ) = (Volt(mV) * Current(mA))/1000
-		Global_f64RMSPower_mA = Global_f64SumVoltage_mv * Global_f64RMSCurrent_mA / 1000;
+//		Global_f64RMSPower_mA = Global_f64SumVoltage_mv * Global_f64RMSCurrent_mA / 1000;
 		Global_f64Energy_mJ += (Global_f64RMSPower_mA * SMAPLING_PERIOD_mS)/1000; // energy = power * time , since this is calculated every ms time = 1/1000
 		
 		Local_u16Count = 0;
+		TIM0_voidSetPreValue(232);
 	}
 
 	Local_u16Count++;
-	TIM0_voidSetPreValue(232);
 }
 
 void CALC_voidReset(void){
 	Global_f64Energy_mJ = 0;
 }
 
+f64 CALC_f64GetInstVoltage_mV(void)	{
+	return Global_f64RMSVoltage_mv;
+}
 
+f64 CALC_f64GetInstCurrent_mA(void){
+	return Global_f64RMSCurrent_mA;
+}
+
+f64 CALC_f64GetInstPower_mW(void){
+	return Global_f64InstPower_mW;
+}
+
+f64 CALC_f64GetEnergy_mJ(void){
+	return Global_f64Energy_mJ;
+}
+
+/*
 f64 CALC_f64GetRMSVoltage_mV(void)	{
 	return Global_f64RMSVoltage_mv;
 }
@@ -83,15 +99,10 @@ f64 CALC_f64GetRMSCurrent_mA(void){
 	return Global_f64RMSCurrent_mA;
 }
 
-f64 CALC_f64GetInstPower_mW(void){
-	return Global_f64InstPower_mW;
-}
-
 f64 CALC_f64GetRMSPower_mW(void){
 	return Global_f64RMSPower_mA;
 }
 
-f64 CALC_f64GetEnergy_mJ(void){
-	return Global_f64Energy_mJ;
-}
+*/
+
 
--- APP/calculations.h
+++ APP/calculations.h
@@ -15,14 +15,19 @@ void CALC_voidPeriodicFunc(void);
 
 void CALC_voidReset(void);
 
+f64 CALC_f64GetInstPower_mW(void);
+
+f64 CALC_f64GetInstVoltage_mV(void);
+
+f64 CALC_f64GetInstCurrent_mA(void);
+
+f64 CALC_f64GetEnergy_mJ(void);
+
+/*
 f64 CALC_f64GetRMSVoltage_mV(void);
 
 f64 CALC_f64GetRMSCurrent_mA(void);
 
-f64 CALC_f64GetInstPower_mW(void);
-
 f64 CALC_f64GetRMSPower_mW(void);
-
-f64 CALC_f64GetEnergy_mJ(void);
-
+*/
 #endif
